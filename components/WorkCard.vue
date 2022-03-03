2e0ef8bd0a442de90b929d9bc4281b05025d9cfc
k.shimamura <34276109+KeisukeShimamura@users.noreply.github.com>
2022-03-03 23:08:08 +0900

    WORKカードをコンポーネント化

---
+++ components/WorkCard.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="card">
+    <div class="card-image overflow-hidden w-full h-64 relative">
+      <img
+        class="w-full h-64 object-cover transition duration-300"
+        :src="imageurl"
+        :alt="imagealt"
+      />
+    </div>
+    <div class="px-6 py-4 pb-2">
+      <div class="font-bold text-lg mb-2">{{ title }}</div>
+      <div>
+        <small>{{ body }}</small>
+      </div>
+    </div>
+    <div class="py-3">
+      <span
+        v-for="s in skills"
+        :key="s"
+        class="inline-block bg-yellow-100 rounded-full mx-1 px-3 py-1 text-sm font-semibold text-yellow-500"
+        >{{ s }}</span
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: () => "",
+    },
+    imageurl: {
+      type: String,
+      default: () => "",
+    },
+    imagealt: {
+      type: String,
+      default: () => "",
+    },
+    body: {
+      type: String,
+      default: () => "",
+    },
+    skills: {
+      type: Array,
+      default: () => {},
+    },
+  },
+}
+</script>
+
+<style scoped>
+.card {
+  @apply max-w-lg rounded-md overflow-hidden shadow-lg mb-4 bg-white cursor-pointer;
+}
+.card:hover .card-image img {
+  @apply scale-110;
+}
+.card .card-image::after {
+  content: "Read More";
+  background: rgba(0, 0, 0, 0.4);
+  padding-top: 35%;
+  @apply text-white opacity-0 absolute w-full h-full top-0 left-0 text-center text-xl;
+}
+.card:hover .card-image::after {
+  @apply opacity-100;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -34,31 +34,15 @@
         <div
           v-for="w in works.contents"
           :key="w.id"
-          class="p-5 flex-none lg:w-1/3 md:w-1/2 sm:w-1/2 w-full transition duration-300 z-10"
+          class="p-5 lg:w-1/3 md:w-1/2 sm:w-1/2 w-full"
         >
-          <div class="max-w-lg rounded overflow-hidden shadow-lg mb-4">
-            <div class="overflow-hidden w-full h-64">
-              <img
-                class="w-full h-64 object-cover"
-                :src="w.image.url"
-                :alt="w.name"
-              />
-            </div>
-            <div class="px-6 py-4 pb-2 bg-white">
-              <div class="font-bold text-xl mb-2">{{ w.name }}</div>
-              <div>
-                <small>{{ w.body }}</small>
-              </div>
-            </div>
-            <div class="py-3">
-              <span
-                v-for="s in w.skill"
-                :key="s"
-                class="inline-block bg-yellow-100 rounded-full px-3 py-1 text-sm font-semibold text-yellow-500 hover:bg-yellow-200"
-                >{{ s }}</span
-              >
-            </div>
-          </div>
+          <WorkCard
+            :title="w.name"
+            :imageurl="w.image.url"
+            :imagealt="w.name"
+            :body="w.body"
+            :skills="w.skill"
+          ></WorkCard>
         </div>
       </div>
     </section>
